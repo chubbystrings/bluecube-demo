32c2498cb1414e600bb3ead6fc5a44052b7a3de4
chubbystrings <48825855+chubbystrings@users.noreply.github.com>
2021-11-12 09:52:35 +0100

    adjusted notification icon

--- src/app/api/unsplash.ts
+++ src/app/api/unsplash.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID TfZ7SvslTU7A9vubmvyMAtqwpk9U3mKZMAb5ySI3pHg"
+        Authorization: `Client-ID ${process.env.REACT_APP_CLIENTID as string}` 
     }
 })
